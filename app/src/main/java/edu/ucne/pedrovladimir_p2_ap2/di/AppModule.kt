fec0660c2be9f4d27082f30d54ce2df0bbfc8c0f
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-02-25 22:34:52 -0400

    Modificamos AppModule.kt

--- app/src/main/java/edu/ucne/pedrovladimir_p2_ap2/di/AppModule.kt
+++ app/src/main/java/edu/ucne/pedrovladimir_p2_ap2/di/AppModule.kt
@@ -1,18 +1,30 @@
 package edu.ucne.pedrovladimir_p2_ap2.di
 
 import android.content.Context
+import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.pedrovladimir_p2_ap2.data.local.database.AppDataBase
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
-class AppModule {
+object AppModule {
     @Provides
     @Singleton
     fun ProvideContex(@ApplicationContext AppContex: Context) =
-        AppContex
+        Room.databaseBuilder(
+            AppContex,
+            AppDataBase::class.java,
+            "App.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+    @Provides
+    @Singleton
+    fun ProvideTDao(appDataBase: AppDataBase) = appDataBase.tDao()
+
+
 }

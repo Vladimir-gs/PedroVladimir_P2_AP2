35ff10f538831e1fe64bbc88a2cb215f41f7a800
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-02-26 20:35:59 -0400

    Modificamos AppNavHost.kt

--- app/src/main/java/edu/ucne/pedrovladimir_p2_ap2/presentation/navigation/AppNavHost.kt
+++ app/src/main/java/edu/ucne/pedrovladimir_p2_ap2/presentation/navigation/AppNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import edu.ucne.pedrovladimir_p2_ap2.presentation.depositos.DepositoList
 import edu.ucne.pedrovladimir_p2_ap2.presentation.home.Home
 
 @Composable
@@ -18,6 +19,14 @@ fun AppNavHost(
         composable<Screen.Home> {
             Home(
                 navController = navController
+            )
+        }
+
+        composable<Screen.DepList> {
+            DepositoList(
+                onCreate = { navController.navigate(Screen.Deposito(0)) },
+                onEdit = {navController.navigate(Screen.DepEdit(it)) },
+                onBack = { navController.navigateUp() }
             )
         }
     }
